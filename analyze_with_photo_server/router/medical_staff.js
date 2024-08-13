e704e32734e7d62bf0ca9585e27c8a76afbda07e
MouGam <88566221+MouGam@users.noreply.github.com>
2024-08-13 16:04:45 +0900

    .

--- analyze_with_photo_server/router/medical_staff.js
+++ analyze_with_photo_server/router/medical_staff.js
@@ -46,9 +46,9 @@ router.post('/requestCounsels', async(req, res, next)=>{
 router.post('/requestCounselsPython', async(req, res, next)=>{
     try{
         const counselList  = await getCounsels();
-        // const _counselList = await getCounselFromFolder(counselList);
+        const _counselList = await getCounselFromFolder(counselList);
         // console.log(counselList)
-        res.json({result:0, data:{counselList:counselList}});
+        res.json({result:0, data:{counselList:_counselList}});
     }catch(err){
         console.error(err);
         res.json({result:2, message:err});
